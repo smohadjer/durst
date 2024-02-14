78e9538c790b605b6828cf80a21ab2ad6449804e
Saeid Mohadjer <saeid@fastmail.fm>
2024-02-14 17:53:40 +0100

    test

--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).json({message: 'hello from express server'});
+    res.status(200).json({message: 'hello from express.js server'});
 });
 
 module.exports = router;
