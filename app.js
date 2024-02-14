f8c36131fa4ed56a37eb5a6ab3a048d4d39823d3
Saeid Mohadjer <saeid@fastmail.fm>
2024-02-14 18:24:33 +0100

    minor

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+console.log(port);
 
-app.use(express.static('public'));
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
-  res.send('Home')
-})
+// app.get('/', function (req, res) {
+//   res.send('Home')
+// })
 
 app.use('/api', router);
 
